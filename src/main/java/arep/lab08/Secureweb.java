d91659e7888ef2fdcab32cb2c75b7d9f69c142e3
unknown <yojhan.toro-r@labinfo.is.escuelaing.edu.co>
2026-03-18 12:15:40 -0500

    Trabajo en clase

---
+++ src/main/java/arep/lab08/Secureweb.java
@@ -0,0 +1,15 @@
+package arep.lab08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class Secureweb {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Secureweb.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
+                app.run(args);
+    }
+}
